34f9c579204fc7f5efd34ded954fbb3320ead115
Dmitriy Isaev <isai1994@mail.ru>
2024-08-17 00:24:30 +0300

    initial commit

---
+++ MyNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNotes
+//
+//  Created by Дмитрий Исаев on 13.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
